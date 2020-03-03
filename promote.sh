de834f5c876d8fba5ba14f0a00eb836a9ed71a0e
James Strachan <james.strachan@gmail.com>
2020-03-03 09:06:19 +0000

    fix: try fix upgrade of enhancements pipelien

--- promote.sh
+++ promote.sh
@@ -19,23 +19,16 @@ git config --global --add user.email jenkins-x@googlegroups.com
 
 git clone https://github.com/jenkins-x/jx-docs.git
 
-pushd jx-docs
-  git checkout -b $LOCAL_BRANCH_NAME
-popd
-
 MESSAGE="chore: updated enhancements content"
 
 pushd jx-docs/content/en/docs/labs/enhancements
   git checkout master
   git pull
-  cd ..
-  git add *
-  git commit --allow-empty -a -m "$MESSAGE"
 popd
 
 pushd jx-docs
-  git push origin $LOCAL_BRANCH_NAME
-  jx create pullrequest -t "$MESSAGE" -l updatebot
-popd
+  git add *
+  git commit --allow-empty -a -m "$MESSAGE"
 
-echo "created Pull Request"
+  git push origin master
+popd
